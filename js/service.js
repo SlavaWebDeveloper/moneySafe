434e8116401f1f7827e52b47a896922713ae2604
Slava_Frondend <sidorov-vv3@gmail.com>
2023-11-10 00:03:49 +0800

    Разбиение js файла по модулям

---
+++ js/service.js
@@ -0,0 +1,37 @@
+const API__URL = "https://overjoyed-wry-paranthodon.glitch.me/api";
+
+export const getData = async (url) => {
+  try {
+    const response = await fetch(`${API__URL}${url}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Ошибка в получении данных:", error);
+    throw error;
+  }
+};
+
+export const postData = async (url, data) => {
+  try {
+    const response = await fetch(`${API__URL}${url}`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Ошибка в отправке данных:", error);
+    throw error;
+  }
+};
