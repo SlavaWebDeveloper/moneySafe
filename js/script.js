7753612456eb8e9ed41b6344b6ceb5dae2518fb4
Slava_Frondend <sidorov-vv3@gmail.com>
2023-11-09 18:39:18 +0800

    Изменение функции открытия и закрытия отчета с помощью GSAP

--- js/script.js
+++ js/script.js
@@ -59,13 +59,30 @@ const closeReport = ({ target }) => {
     target.closest(".report__close") ||
     (!target.closest(".report") && target !== financeReport)
   ) {
-    report.classList.remove("report__open");
+    gsap.to(report, {
+      opacity: 0, 
+      scale: 0,
+      duration: 0.5,
+      eases: 'power2.in',
+      onComplete() {
+        report.style.visibility = "hidden";  
+      }
+    });
+       
     document.removeEventListener("click", closeReport);
   }
 };
 
 const openReport = () => {
-  report.classList.add("report__open");
+  report.style.visibility = "visible";
+
+  gsap.to(report, {
+    opacity: 1, 
+    scale: 1,
+    duration: 0.5,
+    eases: 'power2.out',
+  });
+
   document.addEventListener("click", closeReport);
 };
 
@@ -102,9 +119,15 @@ const renderReport = (data) => {
 };
 
 financeReport.addEventListener("click", async () => {
-  openReport();
+  const textContent = financeReport.textContent;
+  financeReport.textContent = "Загружаю ...";
+  financeReport.disabled = true;
+
   const data = await getData("/test");
+  financeReport.textContent = textContent;
+  financeReport.disabled = false;
   renderReport(data);
+  openReport();
 });
 
 reportDates.addEventListener("submit", async (event) => {
